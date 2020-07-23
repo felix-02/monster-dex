f4846d78819a216d1fb4f0b05a8f9acca7a2d2b2
pulkit <pulkit1298@gmail.com>
2020-07-24 01:35:30 +0530

    Updates

---
+++ precache-manifest.b36d0ae7c22ac2b62c8aa103a258ddf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6dd8938873fd9bea6d46484ff013dbe",
+    "url": "/monster-dex/index.html"
+  },
+  {
+    "revision": "e5821da607f1ccf3b6e9",
+    "url": "/monster-dex/static/css/main.f420a55e.chunk.css"
+  },
+  {
+    "revision": "adfd64caa1676924e4f3",
+    "url": "/monster-dex/static/js/2.35115211.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-dex/static/js/2.35115211.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5821da607f1ccf3b6e9",
+    "url": "/monster-dex/static/js/main.598c8644.chunk.js"
+  },
+  {
+    "revision": "2c453300904bb5e01ddb",
+    "url": "/monster-dex/static/js/runtime-main.ed590583.js"
+  }
+]);
